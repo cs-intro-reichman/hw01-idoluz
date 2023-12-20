d56da36e20fd19316324e366e468bfc84c15610c
idoluz <154246273+idoluz@users.noreply.github.com>
2023-12-20 17:25:55 +0200

    Update GenThree.java

--- GenThree.java
+++ GenThree.java
@@ -4,6 +4,19 @@
  */
 public class GenThree {
 	public static void main(String[] args) {
-		// Put your code here	
+		int N = Integer.parseInt(args[0]); 
+      double r = Math.random();
+         int n = (int) (r * N);
+      double r2 = Math.random();
+         int n2 = (int) (r2 * N);
+      double r3 = Math.random();
+         int n3 = (int) (r3 * N);
+           System.out.println("random integers are");
+           System.out.println(n);
+           System.out.println(n2);
+           System.out.println(n3);
+             int min = Math.min(n,n2);
+                min = Math.min(min,n3);
+                   System.out.println( "the smallest int is " + min); 	
 	}
 }
